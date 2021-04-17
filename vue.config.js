0fbbd3afe7059f6794c31fe3b39ed727f9be4a31
Rebecca <rebj@Rebeccas-MacBook-Pro.local>
2021-04-17 14:07:27 -0400

    updating yml

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/rebjane.github.io" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "rebjane.github.io" : "/",
 };
