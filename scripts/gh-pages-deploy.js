5e63ae65d8003817f44ce755cef7c0f6d1bc18c8
Rebecca <rebj@Rebeccas-MacBook-Pro.local>
2021-04-29 11:58:50 -0400

    trying

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -3,7 +3,7 @@ const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
-    await execa("git", ["checkout", "--orphan", "main"]);
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
@@ -12,7 +12,7 @@ const fs = require("fs");
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "main"]);
-    console.log("Pushing to main...");
+    console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:main", "--force"]);
     await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "main"]);
