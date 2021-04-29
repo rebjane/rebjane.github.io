1e4d28984faa186a3f3ea621f684de7e1bccd99e
Rebecca <rebj@Rebeccas-MacBook-Pro.local>
2021-04-29 12:58:41 -0400

    remove gh pages

---
+++ src/router.js
@@ -0,0 +1,28 @@
+﻿import Vue from "vue";
+import VueRouter from "vue-router";
+
+// Because Vue is not global we tie Vue
+// and VueRouter together manually
+Vue.use(VueRouter);
+
+// Initialize router
+const router = new VueRouter({
+  base: "",
+  mode: "history",
+  routes: [
+    {
+      path: "",
+      component: () => import("./MainPage.vue"),
+    },
+    {
+      path: "/*/:slug",
+      component: () => import("./PageWork.vue"),
+    },
+    {
+      path: "*",
+      component: () => import("./MainPage.vue"),
+    },
+  ],
+});
+
+export default router;
