b359ebb68b6a3985e3bb0933ef3b72918f749514
Rebecca <rebj@Rebeccas-MacBook-Pro.local>
2021-05-15 10:27:43 -0400

    removed history mode in router

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "rebj2021" : "rebj2021",
-  outputDir: "rebj2021",
+  publicPath: process.env.NODE_ENV === "production" ? "rebj" : "/",
+  outputDir: "rebj",
   // ...
 };
