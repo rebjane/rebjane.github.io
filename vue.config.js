f6a8da3318921d22533939375f928121e347df25
Rebecca <rebj@Rebeccas-MacBook-Pro.local>
2021-05-15 09:16:19 -0400

    troubleshooting

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "rebj2021" : "rebj2021",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "rebj.design" : "rebj.design",
   outputDir: "rebj.design",
   // ...
 };
