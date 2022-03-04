dcdea7d4aacfad42d7e2cc8234ddbe8719a15bd2
Rebecca <rebj@Rebeccas-MacBook-Pro.local>
2022-03-04 14:35:09 -0500

    bank page

--- src/Bank.vue
+++ src/Bank.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="bank-list">
-    <Nav :winresize="winresize" />
+    <h1>Bank Links</h1>
     <div class="links">
       <transition v-for="(item, i) in $bank.banks" :key="i">
-        <div class="link">
-          <img :src="item.logo.url" />
-        </div>
+        <a :href="$cms.textField(item.link)" target="_blank">
+          <div
+            class="link heart"
+            :style="`background-image: url(${item.logo.url})`"
+          ></div>
+        </a>
       </transition>
+    </div>
+    <div class="disclaimer">
+      <p>
+        The sole purpose of this webpage is to make the payment process quicker
+        and more accessible, directly from the issued invoice itself.
+      </p>
+      <p>
+        Please note that these are simply external links to the banks only, and
+        that your connection will be completely separate and secure from this
+        website.
+      </p>
     </div>
   </div>
 </template>
@@ -37,19 +51,52 @@ export default {
 </script>
 
 <style lang="scss">
-#bank {
+#bank-list {
   overscroll-behavior: none;
   color: white;
-}
-img {
-  width: 100%;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  padding-left: 10%;
+  padding-right: 10%;
+  background: lightgray;
+  min-height: calc(100vh - 200px);
 }
 .link {
   display: inline-block;
-  width: 200px;
+  vertical-align: middle;
+  width: 150px;
+  height: 100px;
   padding: 0 20px;
-  margin: 0 10px;
   background: white;
-  border-radius: 20px;
+  margin: 0 10px;
+  border-radius: 10px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  margin: 10px;
+  -webkit-box-shadow: 0px 0px 17px 6px rgba(0, 0, 0, 0.28);
+  box-shadow: 0px 0px 17px 6px rgba(0, 0, 0, 0.28);
+}
+h1 {
+  color: black;
+  font-size: 40px;
+}
+.disclaimer {
+  padding-top: 30px;
+  width: 50%;
+  margin: auto;
+  p {
+    color: black;
+    font-family: Helvetica;
+  }
+}
+@media screen and (max-width: 1000px) {
+  .disclaimer {
+    width: 100%;
+  }
+  #bank-list {
+    padding-left: 5%;
+    padding-right: 5%;
+  }
 }
 </style>
